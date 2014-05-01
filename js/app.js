7867729169923542ab2c3157a937fe681d7cc50f
Mark Lehman & Harland Hauger <student@epicodus.com>
2014-05-01 10:27:52 -0700

    build mini blog app along with ember video

---
+++ js/app.js
@@ -0,0 +1,46 @@
+App = Ember.Application.create();
+
+App.Router.map(function(){
+  this.resource('about');
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+
